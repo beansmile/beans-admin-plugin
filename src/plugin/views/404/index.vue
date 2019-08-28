ebf7c65ce7f7ea1cf3c15d10671a8269818f28f1
dave <dave@beansmile.com>
2019-08-28 10:19:16 +0800

    refactor: use base64 image

---
+++ src/plugin/views/404/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <img @click="goBack" class="one-page-image" :src="IMG404" alt="404-image" />
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  IMG404 = require('./image').default;
+
+  goBack() {
+    this.$router.go(-1)
+  }
+}
+</script>
