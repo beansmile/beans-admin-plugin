767c0a36002e0e8e45f2e0c0c18c0ce34586f5ad
jamie <jamie@beansmile.com>
2019-09-05 10:31:47 +0800

    fix: 七牛上传用的 url，应该使用 api 返回的数据
    对象存储根据地区不同，上传地址是不一样的

--- src/plugin/utils/upload.js
+++ src/plugin/utils/upload.js
@@ -3,12 +3,12 @@ import _ from 'lodash';
 import { request } from './request';
 
 export default async function upload(file) {
-  const { meta_url, upload_url, customUpload } = Vue.appConfig.upload;
+  const { meta_url, customUpload } = Vue.appConfig.upload;
   if (_.isFunction(customUpload)) {
     return customUpload(file);
   }
   const qiniuMeta = await request.get(meta_url, { loading: false });
-  const { bucket_domain, token } = qiniuMeta;
+  const { bucket_domain, token, upload_url = Vue.appConfig.upload.upload_url } = qiniuMeta;
   const formData = new FormData();
   formData.append('file', file);
   formData.append('token', token);
