bcd4059c256225c78ffeb0be66ad7077de88648d
duwei <905571995@qq.com>
2020-04-22 12:25:42 +0800

    feat: AppLayout组件添加toolbar-right插槽

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import i18n from './i18n-import'
-import BeansAdminPlugin, { AppLayout } from './plugin';
+import BeansAdminPlugin from './plugin';
 import router from './router';
 import './theme.scss';
 import './components';
@@ -11,5 +11,5 @@ Vue.use(BeansAdminPlugin, { router });
 
 new Vue({
   router, i18n,
-  render: h => h(AppLayout),
+  render: h => h(require('./app').default),
 }).$mount('#app')
