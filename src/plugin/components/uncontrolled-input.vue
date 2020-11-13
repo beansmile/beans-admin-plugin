386e9333836265814052dde050eb2af646027e58
duwei <905571995@qq.com>
2020-11-13 10:17:23 +0800

    feat: add uncontrolled input component

---
+++ src/plugin/components/uncontrolled-input.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-input
+    v-model="inputValue"
+    @change="handleChange"
+    v-bind="$attrs"
+  />
+</template>
+
+<script>
+  import { Vue, Component, Watch, Model, Emit } from 'vue-property-decorator';
+
+  @Component
+  export default class AdminUncontrolledInput extends Vue {
+    @Model('change', { type: [Number, String] }) value;
+
+    inputValue = '';
+
+    @Emit('change')
+    handleChange() {
+      return this.inputValue;
+    }
+
+    @Watch('value', { immediate: true })
+    onValueChange(val) {
+      this.inputValue = val;
+    }
+  }
+</script>
