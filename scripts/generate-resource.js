ee3719096c8aabc0beae2aafda3953b67571b4c6
duwei <905571995@qq.com>
2020-11-18 14:09:49 +0800

    feat: update gen resource

--- scripts/generate-resource.js
+++ scripts/generate-resource.js
@@ -4,11 +4,7 @@ const process = require('process')
 const Mustache = require('mustache')
 const _ = require('lodash')
 const pluralize = require('pluralize')
-const BASE_DIR = 'template'
-const TARGET_DIR = 'src/views'
 const name = process.argv[2]
-
-require('dotenv').config()
 
 if (!name) throw new Error('You must provide the name of resource')
 
@@ -16,7 +12,7 @@ const className = _.upperFirst(_.camelCase(name))
 const resourceName = pluralize.plural(_.snakeCase(name))
 const modelName = _.upperFirst(pluralize.singular(_.camelCase(name)));
 
-const fd = fs.openSync(path.join(BASE_DIR, 'page.mustache'), 'r+')
+const fd = fs.openSync(path.join(__dirname, '../template/page.mustache'), 'r+')
 const content = fs.readFileSync(fd, 'utf8')
 const result = Mustache.render(content, { className, resourceName, modelName }, {}, ['{[', ']}'])
-fs.writeFileSync(path.join(TARGET_DIR, resourceName + '.vue'), result);
+fs.writeFileSync(path.resolve('src/views', resourceName + '.vue'), result);
