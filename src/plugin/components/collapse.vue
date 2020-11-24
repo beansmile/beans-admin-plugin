16716496efdd220a0005b8e2cbdd8fb12e5a418b
dave <dave@beansmile.com>
2020-11-24 16:54:19 +0800

    feat: 多语言表单在同一个表单填写

---
+++ src/plugin/components/collapse.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="admin-collapse">
+    <div class="box-content">
+      <div class="content">
+        <slot />
+      </div>
+      <el-button :icon="`el-icon-arrow-${collapsed ? 'down' : 'up'}`" circle @click="collapsed = !collapsed"></el-button>
+    </div>
+    <div class="box-collapsed" v-show="!collapsed">
+      <slot name="collapsed" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import { Vue, Component } from 'vue-property-decorator';
+
+  @Component
+  export default class Collapse extends Vue {
+    collapsed = true;
+  }
+</script>
+
+<style lang="scss" scoped>
+  .box-content {
+    display: flex;
+    align-items: flex-start;
+
+    .content {
+      flex: 1;
+    }
+
+    .el-button {
+      flex-shrink: 0;
+      margin-left: 15px;
+    }
+  }
+</style>
