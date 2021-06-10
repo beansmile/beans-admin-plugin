36a0db37c4fa901170f45c198323c23310427076
duwei <905571995@qq.com>
2021-06-10 17:11:08 +0800

    feat: import-button支持自定义上传和使用全局上传

--- src/plugin/components/import-button.vue
+++ src/plugin/components/import-button.vue
@@ -16,6 +16,7 @@
   import { Vue, Component, Prop } from 'vue-property-decorator';
   import _ from 'lodash';
   import { Message } from 'element-ui';
+  import { uploadFile } from '../utils/upload';
 
   @Component
   export default class ExportButton extends Vue {
@@ -26,58 +27,73 @@
     @Prop(String) url; // 导入接口路径
     @Prop({ type: String, default: '.xlsx' }) fileAccept;
     @Prop(String) loadingToast;
+    @Prop(Function) customUpload;
+    @Prop({ type: Boolean, default: false }) useGlobalUpload; // 使用全局的upload
+    @Prop({ type: Object }) globalUploadProps;
 
     importing = false;
 
+    download(blob, fileName) {
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.style = 'display: none';
+      a.href = window.URL.createObjectURL(blob);
+      a.download = fileName;
+      a.click();
+      document.body.removeChild(a);
+    }
+
+    async upload(file) {
+      const form = new FormData();
+      form.append('file_message', file);
+      const requestConfig = {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        responseType: 'blob',
+        return_res: true,
+        // 1小时超时
+        timeout: 1 * 60 * 60 * 1000
+      }
+      const result = await this.$request.post(this.url, form, requestConfig);
+      if (result.headers['x-error-message']) {
+        throw new Error(decodeURI(result.headers['x-error-message']));
+      }
+      if (+result.headers['x-error-count'] > 0) {
+        this.download(new Blob([result.data]), `${this.$t('bean.error_massage')}.xlsx`);
+        throw new Error(this.$t('bean.importFail'));
+      }
+    }
+
     async handleFileChange(e) {
       if (_.isFunction(this.fileChangeHandler)) {
-        await this.fileChangeHandler(e);
-      } else {
-        this.xlsxFile = e.target.files[0];
-        if (this.xlsxFile) {
-          this.importing = true;
-          let toastInstance = null;
-          if (this.loadingToast) {
-            toastInstance = Message.info({
-              message: this.loadingToast,
-              duration: 0,
-              showClose: true
-            });
-          }
-          try {
-            let form = new FormData();
-            form.append('file_message', this.xlsxFile);
-            const requestConfig = {
-              headers: { 'Content-Type': 'multipart/form-data' },
-              responseType: 'blob',
-              return_res: true,
-              // 1小时超时
-              timeout: 1 * 60 * 60 * 1000
-            }
-            await this.$request.post(this.url, form, requestConfig).then(
-              (result) => {
-                if (result.headers['x-error-message']) {
-                  this.$message.error(decodeURI(result.headers['x-error-message']));
-                } else if (+result.headers['x-error-count'] > 0) {
-                  const a = document.createElement('a');
-                  document.body.appendChild(a);
-                  a.style = 'display: none';
-                  a.href = window.URL.createObjectURL(new Blob([result.data]));
-                  a.download = `${this.$t('bean.error_massage')}.xlsx`;
-                  a.click();
-                  document.body.removeChild(a);
-                  this.$message.error(this.$t('bean.importFail'));
-                } else {
-                  this.$message.success(this.$t('bean.importSuccess'));
-                }
-              }
-            );
-          } finally {
-            this.importing = false;
-            if (toastInstance) {
-              toastInstance.close();
-            }
-          }
+        this.fileChangeHandler(e);
+        return;
+      }
+      const file = e.target.files[0];
+      this.importing = true;
+      let toastInstance = null;
+      if (this.loadingToast) {
+        toastInstance = Message.info({
+          message: this.loadingToast,
+          duration: 0,
+          showClose: true
+        });
+      }
+      try {
+        if (_.isFunction(this.customUpload)) {
+          await this.customUpload(file);
+        } else if (this.useGlobalUpload) {
+          await uploadFile(file, this.globalUploadProps);
+        } else {
+          await this.upload(file);
+        }
+        this.$message.success(this.$t('bean.importSuccess'));
+      } catch (e) {
+        const msg = e.message || this.$t('bean.importFail');
+        this.$message.error(msg);
+      } finally {
+        this.importing = false;
+        if (toastInstance) {
+          toastInstance.close();
         }
       }
       e.target.value = '';
