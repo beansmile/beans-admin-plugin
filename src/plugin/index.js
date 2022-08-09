1d055dcc276c57b39bb915ea9a0ee05b164d4e1a
colin <colin@beansmile.com>
2022-08-09 16:31:35 +0800

    feat: 添加 环境 提示，防止客户将 staging 环境，误认为是正式环境

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -5,6 +5,7 @@ import installElement from './element';
 import { request, fly, autoLoading } from './utils';
 import MavonEditor from 'mavon-editor';
 import 'mavon-editor/dist/css/index.css';
+import alertEnvTips from './utils/alert-env-tips.js';
 
 export default function install(Vue, options = {}) {
   installElement(Vue);
@@ -30,6 +31,7 @@ export default function install(Vue, options = {}) {
       throw err;
     }
   }
+  alertEnvTips();
 }
 
 export * from './services';
